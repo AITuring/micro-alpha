ae9322ea15e59271bb3ac006f3cb5ef41790c877
lishengyu <lishengyu@baidu.com>
2021-06-29 00:55:24 +0800

    增加新页面,测试子应用新页面打开没有主应用url问题

--- .umirc.ts
+++ .umirc.ts
@@ -1,15 +1,15 @@
-
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-    nodeModulesTransform: {
-        type: 'none',
-    },
-    routes: [
-        { path: '/', component: '@/pages/index' },
-    ],
-    fastRefresh: {},
-    qiankun: {
-        slave: {}
-    }
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index', name: 'index' },
+    { path: '/hello', component: '@/pages/hello', name: 'hello' },
+  ],
+  fastRefresh: {},
+  qiankun: {
+    slave: {},
+  },
 });
---
+++ src/pages/hello.jsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+  return (
+    <>
+      <div>Hello</div>
+      <button onClick={window.open('/', '_blank')}>open</button>
+    </>
+  );
+};
+
+export default Hello;
